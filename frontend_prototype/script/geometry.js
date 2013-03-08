81993cbc7ee428714c9b75044028be90cfb5b843
Christian Speckner <cnspeckn@googlemail.com>
2013-03-08 16:34:39 +0100

    Add missing geometry.js, move to frontend_prototype.

---
+++ frontend_prototype/script/geometry.js
@@ -0,0 +1,33 @@
+define(['lib/underscore', 'util'],
+   function(_, Util)
+{
+   Geometry = {};
+
+   Geometry.Rectangle = Util.extend(Util.Base, {
+      properties: ['x', 'y', 'width', 'height'],
+
+      create: function(config) {
+         var me = this;
+
+         me.getConfig(config, ['x', 'y', 'width', 'height']);
+      },
+
+      isInside: function(x, y) {
+         var me = this;
+
+         return (x >= me._x) && (x < me._x + me._width) &&
+            (y >= me._y) && (y < me._y + me._height);
+      },
+
+      intersect: function(rect) {
+         var me = this;
+
+         return me.isInside(rect._x, rect._y) ||
+            me.isInside(rect._x + rect._width - 1, rect._y) ||
+            me.isInside(rect._x, rect._y + rect._height - 1) ||
+            me.isInside(rect._x + rect._width - 1, rect._y + rect._height - 1);
+      }
+   });
+
+   return Geometry;
+});
