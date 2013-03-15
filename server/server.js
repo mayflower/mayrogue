23673407cb89d82a72a80fd5c1a444b30b492e08
Dominik Liebler <liebler.dominik@googlemail.com>
2013-03-15 10:05:42 +0100

    switched to express

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
-var connect = require('connect');
+var express = require('express');
 
-var app = connect()
-  .use(connect.logger('dev'))
-  .use(connect.static(__dirname + '/../frontend_prototype'))
- .listen(3000);
+var app = express()
+  .use(express.logger('dev'))
+  .use(express.static(__dirname + '/../frontend_prototype'));
+
+
+app.get('/foo', function(req, res) {
+    res.send('foobar\n');
+});
+
+app.listen(3000);
