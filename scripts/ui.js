0c7c3c6316e6a9bbb14d779d028204c10bb1b2f9
Christian Speckner <cnspeckn@googlemail.com>
2013-03-21 07:40:35 +0100

    Whitespace.

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,68 +1,70 @@
+// vim: set softtabstop=4
+
 define(['underscore', 'util'],
-   function(_, Util)
+    function(_, Util)
 {
-   "use strict";
+    "use strict";
 
-   var ui = {};
+    var ui = {};
 
-   ui.MapView = Util.extend(Util.Base, {
-      properties: [
-         {field: '_world', getter: true},
-         {field: '_tiles', getter: true},
-         {field: '_canvas', getter: true}
-      ],
+    ui.MapView = Util.extend(Util.Base, {
+        properties: [
+            {field: '_world', getter: true},
+            {field: '_tiles', getter: true},
+            {field: '_canvas', getter: true}
+        ],
 
-      _context: null,
+        _context: null,
 
-      create: function(config) {
-         var me = this;
+        create: function(config) {
+            var me = this;
 
-         me.getConfig(config,
-            ['world', 'tiles', 'canvas']);
+            me.getConfig(config,
+                ['world', 'tiles', 'canvas']);
 
-         me._world.attachListeners({visibleChange: me.redraw}, me);
+            me._world.attachListeners({visibleChange: me.redraw}, me);
 
-         me._canvas.width = me._tiles.width * me._world.getViewport().getWidth();
-         me._canvas.height = me._tiles.height * me._world.getViewport().getHeight();
-         me._context = me._canvas.getContext('2d');
+            me._canvas.width = me._tiles.width * me._world.getViewport().getWidth();
+            me._canvas.height = me._tiles.height * me._world.getViewport().getHeight();
+            me._context = me._canvas.getContext('2d');
 
-         me.redraw();
-      },
+            me.redraw();
+        },
 
-      destroy: function() {
-         var me = this;
+        destroy: function() {
+            var me = this;
 
-         me._world.detachListeners({visibleChange: me.redraw}, me);
-      },
+            me._world.detachListeners({visibleChange: me.redraw}, me);
+        },
 
-      redraw: function() {
-         var me = this;
-         var x, y;
-         var mapData = me._world.getMapData();
-         var viewport = me._world.getViewport();
-         var x0 = viewport.getX(), y0 = viewport.getY();
+        redraw: function() {
+            var me = this;
+            var x, y;
+            var mapData = me._world.getMapData();
+            var viewport = me._world.getViewport();
+            var x0 = viewport.getX(), y0 = viewport.getY();
 
-         for (x = x0; x < x0 + viewport.getWidth(); x++)
-            for (y = y0; y < y0 + viewport.getHeight(); y++)
+            for (x = x0; x < x0 + viewport.getWidth(); x++)
+                for (y = y0; y < y0 + viewport.getHeight(); y++)
 
-               me._tiles.drawTo(
-                  me._context,
-                  me._tiles.width * (x - x0),
-                  me._tiles.height * (y - y0),
-                  mapData[x][y]
-               );
+                    me._tiles.drawTo(
+                        me._context,
+                        me._tiles.width * (x - x0),
+                        me._tiles.height * (y - y0),
+                        mapData[x][y]
+                    );
 
-         _.each(me._world.getEntities(), function(entity) {
+            _.each(me._world.getEntities(), function(entity) {
 
-            me._tiles.drawTo(
-               me._context,
-               me._tiles.width * (entity.getX() - x0),
-               me._tiles.height * (entity.getY() - y0),
-               entity.getShape()
-            );
-         });
-      }
-   });
+                me._tiles.drawTo(
+                    me._context,
+                    me._tiles.width * (entity.getX() - x0),
+                    me._tiles.height * (entity.getY() - y0),
+                    entity.getShape()
+                );
+            });
+        }
+    });
 
-   return ui;
+    return ui;
 });
