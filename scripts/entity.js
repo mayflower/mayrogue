8186fb075d9f62d49a8553db3967ffb736fa32ef
Alberto Assmann <newloki91@googlemail.com>
2013-05-11 14:55:18 +0200

    fix: add missing getter/setter for world property on entities

--- scripts/entity.js
+++ scripts/entity.js
@@ -13,7 +13,8 @@ define(['underscore', 'util', 'geometry', 'tiles', 'stats'],
             'heading',
             {field: '_id', getter: true},
             {field: '_boundingBox', getter: true},
-            {field: '_stats', getter: true}
+            {field: '_stats', getter: true},
+            {field: '_world', getter: true, setter: true}
         ],
         mixins: [Util.Observable],
 
